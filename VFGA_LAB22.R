b43955bef012446c3a839da52a0a5cd4c926a817
Verónica F. García Arteaga <69220883+Vero-arte@users.noreply.github.com>
2020-09-14 18:17:50 -0500

    Create VFGA_LAB22.R

---
+++ VFGA_LAB22.R
@@ -0,0 +1,57 @@
+###################
+#                  #
+# Copiar todo esto #
+#                  #
+####################
+# Hecho con gusto por Veronica F. Garcia Arteaga
+#V9
+# LABORATORIO - Gráficos en R con ggplot2 para Ciencia de Datos
+# editar con temas de ggplot2
+
+
+
+#instalando paquete con los datos
+#install.packages("gapminder")
+#install.packages("ggplot2")
+
+
+
+# cargar libreria ggplot2 y gapminder
+library(ggplot2)
+library(gapminder)
+
+# cargando datos a entorno
+data("gapminder")
+
+# filtrando por año 2007
+gapminder2007 <- gapminder[gapminder$year == '2007', ]
+
+
+
+
+# haciendo grafica de puntos
+p1 <- ggplot(data = gapminder2007,
+             mapping = aes(x = gdpPercap,
+                           y = lifeExp,
+                           color = continent)) +
+  geom_point()
+
+p1
+
+
+
+# temas básicos
+p1 + theme_bw()
+p1 + theme_minimal()
+
+# mas temas
+install.packages('ggthemes')
+library(ggthemes)
+
+# temas y colores
+p1 + theme_economist() + scale_color_economist()
+p1 + theme_excel_new() + scale_color_excel_new()
+p1 + theme_solid() + scale_color_calc()
+p1 + theme_solarized() + scale_color_solarized()
+p1 + theme_fivethirtyeight() + scale_color_fivethirtyeight()
+p1 + theme_foundation() + scale_colour_few()
